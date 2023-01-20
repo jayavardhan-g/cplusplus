a096c34831dc843b5cbe20f74222a8f93dbbb180
jayavardhan-nani <jayavardhan.gollapalli@gmail.com>
2023-01-20 10:28:51 +0530

    20-01-2023

--- Tools/GPA.cpp
+++ Tools/GPA.cpp
@@ -3,10 +3,19 @@ using namespace std;
 #define int long long
 int32_t main(){
     double x,y;
+    string s;
     double a;
     double i=0,j=0;
     while(x!=-1){
-        cin>>x>>y;
+        cin>>x;
+        cin>>s;
+        if(s=="A+")y=10;
+        else if(s=="A")y=9;
+        else if(s=="B+")y=8;
+        else if(s=="B")y=7;
+        else if(s=="C+")y=6;
+        else if(s=="C")y=5;
+        else if(s=="D")y=4;
         if(x==-1)break;
         x/=2;
         i+=x;
@@ -14,7 +23,15 @@ int32_t main(){
     }
     x=0;
     while(x!=-1){
-        cin>>x>>y;
+        cin>>x;
+        cin>>s;
+        if(s=="A+")y=10;
+        else if(s=="A")y=9;
+        else if(s=="B+")y=8;
+        else if(s=="B")y=7;
+        else if(s=="C+")y=6;
+        else if(s=="C")y=5;
+        else if(s=="D")y=4;
         if(x==-1)break;
         i+=x;
         j+=x*y;
@@ -22,3 +39,55 @@ int32_t main(){
     a=j/i;
     cout<<a;
 }
+
+/*
+6 C+
+6 C+
+11 B
+11 B+
+9 C
+4.5 A
+4.5 C+
+1.5 A
+1.5 A
+2 B
+
+9 C+
+5 B+
+9 B+
+11 B+
+4 B+
+1 C+
+3 B
+3 A
+5 B+
+2 A
+
+9 C
+9 C
+9 C+
+9 C
+11 C
+2 C+
+2 C+
+
+-1 X
+
+9 D
+9 C
+9 C
+9 C+
+9 B+
+2 B
+2 A
+
+9 D
+9 D
+9 B
+9 C
+9 B
+2 B+
+2 B
+
+-1 X
+*/
