a5453930a14c29afb05edd2692aebc7edc40bde5
Jayavardhan <jayavardhan.gollapalli@gmail.com>
2023-06-22 22:49:26 +0530

    22-06-2023

--- Striver/DP/Test.cpp
+++ Striver/DP/Test.cpp
@@ -1,50 +1,28 @@
-int maximumChocolates(int n, int m, vector < vector < int >> & grid) {
-  // Write your code here.
-  vector < vector < vector < int >>> dp(n, vector < vector < int >> (m, 
-  vector < int > (m, 0)));
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
 
-  for (int j1 = 0; j1 < m; j1++) {
-    for (int j2 = 0; j2 < m; j2++) {
-      if (j1 == j2)
-        dp[n - 1][j1][j2] = grid[n - 1][j1];
-      else
-        dp[n - 1][j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
-    }
-  }
+        long long int t;
+       cin>>t;
+       while(t--){
+       long long int n,m,h,ans=0,i;
+       cin>>n >>m >>h;
 
-  //Outer Nested Loops for travering DP Array
-  for (int i = n - 2; i >= 0; i--) {
-    for (int j1 = 0; j1 < m; j1++) {
-      for (int j2 = 0; j2 < m; j2++) {
-
-        int maxi = INT_MIN;
-
-        //Inner nested loops to try out 9 options
-        for (int di = -1; di <= 1; di++) {
-          for (int dj = -1; dj <= 1; dj++) {
-
-            int ans;
-
-            if (j1 == j2)
-              ans = grid[i][j1];
-            else
-              ans = grid[i][j1] + grid[i][j2];
-
-            if ((j1 + di < 0 || j1 + di >= m) ||
-              (j2 + dj < 0 || j2 + dj >= m))
-
-              ans += -1e9;
-            else
-              ans += dp[i + 1][j1 + di][j2 + dj];
-
-            maxi = max(ans, maxi);
-          }
-        }
-        dp[i][j1][j2] = maxi;
-      }
-    }
-  }
-
-  return dp[0][0][m - 1];
-
+        long long int p=min(n,m);
+       vector<long long int>a(n),b(m);
+       for(i=0;i<n;i++){
+       cin>>a[i] ;
+       }
+       for(i=0;i<m;i++){
+       cin>>b[i] ;
+       }
+       sort(a.begin(),a.end());
+       reverse(a.begin(),a.end());
+       sort(b.begin(),b.end());
+       reverse(b.begin(),b.end());
+       for(i=0;i< p;i++){
+       ans=ans+ min(a[i],h*b[i]);
+       }
+       cout<<ans<<endl;
+       }
 }
